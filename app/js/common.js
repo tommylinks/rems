4823a6c0f9bf89407a65eef59d843dede9d5b91a
tommylinks <tommylinks1@gmail.com>
2017-09-08 18:33:45 +0300

    add desktop catalog tiles and sketch of mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
 
-	// Custom JS
+    $('.c-seo-hider').click(function () {
+        $('.c-seo').toggleClass('show-all');
+    });
 
+
+    function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('up');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
 });
